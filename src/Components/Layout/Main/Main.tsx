190b6a1ee64fc61541158fa6b27dd88f8c98dc7a
Shalev140 <112413822+Shalev140@users.noreply.github.com>
2023-04-04 15:46:56 +0300

    add page not found 404

--- src/Components/Layout/Main/Main.tsx
+++ src/Components/Layout/Main/Main.tsx
@@ -4,6 +4,7 @@ import Dashboard from "./Dashboard/Dashboard";
 import Games from "./Dashboard/Games/Games";
 import Home from "./Home/Home";
 import "./Main.css";
+import PageNotFound from "./PageNotFound/PageNotFound";
 
 function Main(): JSX.Element {
     return (
@@ -15,7 +16,7 @@ function Main(): JSX.Element {
                 <Route path="/dashboard" element={<Dashboard/>}></Route>
                 <Route path="/games" element={<Games/>}></Route>
                 <Route path="/articles" element={<Articles/>}></Route>
-                {/* <Route path="/*" element={<PageNotFound/>}></Route> */}
+                <Route path="/*" element={<PageNotFound/>}></Route>
 			</Routes>
         </div>
     );

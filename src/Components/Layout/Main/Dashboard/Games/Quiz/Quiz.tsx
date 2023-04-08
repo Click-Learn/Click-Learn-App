deed78a56619b904061f42b431d05f918008767f
harelsarag7 <112413505+harelsarag7@users.noreply.github.com>
2023-04-08 13:57:23 +0300

    fixed quiz game problem

--- src/Components/Layout/Main/Dashboard/Games/Quiz/Quiz.tsx
+++ src/Components/Layout/Main/Dashboard/Games/Quiz/Quiz.tsx
@@ -1,81 +1,79 @@
 
+
 import React, { useState, useEffect } from "react";
 import "./Quiz.css";
 
 interface Word {
-englishWord: string;
-hebrewWord: string;
+  englishWord: string;
+  hebrewWord: string;
 }
 
-
 const words: Word[] = [
-{ englishWord: "hello", hebrewWord: "שלום" },
-{ englishWord: "goodbye", hebrewWord: "להתראות" },
-{ englishWord: "please", hebrewWord: "בבקשה" },
-{ englishWord: "thank you", hebrewWord: "תודה" },
-{ englishWord: "sun", hebrewWord: "שמש" },
-{ englishWord: "hat", hebrewWord: "כובע" },
-// Add more card data as needed
+  { englishWord: "hello", hebrewWord: "שלום" },
+  { englishWord: "goodbye", hebrewWord: "להתראות" },
+  { englishWord: "please", hebrewWord: "בבקשה" },
+  { englishWord: "thank you", hebrewWord: "תודה" },
+  { englishWord: "sun", hebrewWord: "שמש" },
+  { englishWord: "hat", hebrewWord: "כובע" },
+  // Add more card data as needed
 ];
 
 const Quiz: React.FC = () => {
-const [currentWordIndex, setCurrentWordIndex] = useState(0);
-const [isCorrectAnswer, setIsCorrectAnswer] = useState<boolean | null>(null);
-const [timer, setTimer] = useState(10);
-const [hasAnswered, setHasAnswered] = useState(false);
-const [isTimerRunning, setIsTimerRunning] = useState(true);
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const [isCorrectAnswer, setIsCorrectAnswer] = useState<boolean | null>(null);
+  const [timer, setTimer] = useState(10);
+  const [hasAnswered, setHasAnswered] = useState(false);
+  const [isTimerRunning, setIsTimerRunning] = useState(true);
 
-useEffect(() => {
-let intervalId: NodeJS.Timeout;
-if (isTimerRunning) {
-intervalId = setInterval(() => {
-if (timer > 0) {
-setTimer((prevTimer) => prevTimer - 1);
-} else {
-setIsTimerRunning(false);
-setIsCorrectAnswer(false);
-setHasAnswered(true);
-}
-}, 1000);
-}
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout;
+    if (isTimerRunning) {
+      intervalId = setInterval(() => {
+        if (timer > 1) {
+          setTimer((prevTimer) => prevTimer - 1);
+        } else {
+          setIsTimerRunning(false);
+          setIsCorrectAnswer(false);
+          setHasAnswered(true);
+        }
+      }, 1000);
+    }
 
-return () => clearInterval(intervalId);
-}, [timer, isTimerRunning]);
+    return () => clearInterval(intervalId);
+  }, [timer, isTimerRunning]);
 
-const handleAnswer = (answerIndex: number) => {
-const isAnswerCorrect = answerIndex === 0;
-setIsCorrectAnswer(isAnswerCorrect);
-setIsTimerRunning(false);
-setHasAnswered(true);
-};
+  const handleAnswer = (answerIndex: number) => {
+    const isAnswerCorrect = answerIndex === shuffledAnswers.indexOf(currentWord.hebrewWord);
+    setIsCorrectAnswer(isAnswerCorrect);
+    setIsTimerRunning(false);
+    setHasAnswered(true);
+  };
 
-const handleNextWord = () => {
-setCurrentWordIndex((prevIndex) => {
-const nextIndex = prevIndex + 1;
-if (nextIndex < words.length) {
-return nextIndex;
-} else {
-return 0;
-}
-});
-setTimer(10);
-setIsCorrectAnswer(null);
-setHasAnswered(false);
-setIsTimerRunning(true);
-};
+  const handleNextWord = () => {
+    setCurrentWordIndex((prevIndex) => {
+      const nextIndex = prevIndex + 1;
+      if (nextIndex < words.length) {
+        return nextIndex;
+      } else {
+        return 0;
+      }
+    });
+    setTimer(10);
+    setIsCorrectAnswer(null);
+    setHasAnswered(false);
+    setIsTimerRunning(true);
+  };
 
-const currentWord = words[currentWordIndex];
-const shuffledAnswers = React.useMemo(() => {
-return [
-currentWord.hebrewWord,
-...words
-.filter((word) => word.englishWord !== currentWord.englishWord)
-.map((word) => word.hebrewWord),
-]
-.sort(() => Math.random() - 0.5)
-.slice(0, 4);
-}, [currentWord, words]);
-
+  const currentWord = words[currentWordIndex];
+  const shuffledAnswers = React.useMemo(() => {
+    const answers = [
+      currentWord.hebrewWord,
+      ...words
+        .filter((word) => word.englishWord !== currentWord.englishWord)
+        .map((word) => word.hebrewWord),
+    ].slice(0, 4);
+    return answers.sort(() => Math.random() - 0.5);
+  }, [currentWord, words]);
 
 
   return (

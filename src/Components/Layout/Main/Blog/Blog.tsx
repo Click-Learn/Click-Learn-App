4e58bfd72bef01d69110f49b9c357f49582c7235
harelsarag7 <112413505+harelsarag7@users.noreply.github.com>
2023-04-10 00:36:10 +0300

    updated blog route

--- src/Components/Layout/Main/Blog/Blog.tsx
+++ src/Components/Layout/Main/Blog/Blog.tsx
@@ -1,27 +1,33 @@
 import "./Blog.css";
 import MainBanner from "./MainBanner.jpg";
+import blog1Image from "./blog1.jpg";
+import blog2Image from "./blog2.jpg";
+import blog3Image from "./blog3.jpg";
+import blog4Image from "./blog4.jpg";
+import { useNavigate } from "react-router-dom";
 
 function Blog(): JSX.Element {
+    const navigate = useNavigate()
     return (
         <div className="Blog">
 			<div className="blog_main_img_container">
                 <img className="main_image"src={MainBanner} alt="" />
             </div>
 			<div className="blog_articles_container">
-                <div className="article">
-                <img className="image_article" src="https://img.freepik.com/free-photo/landscape-tropical-vacation-palm-summer_1203-5352.jpg?w=1380&t=st=1675791517~exp=1675792117~hmac=ef425190945513f93deb4bec94c0bccdca00665a81087668bc44215b5e63a852" alt="" />
-                <p className="article_title">"Escape to Paradise: Ultimate Vacations"</p>
-                <p className="article_author">A guide to the most luxurious vacation destinations.</p>
+                <div className="article" onClick={() => navigate("/blog-post-3")}>
+                <img className="image_article" src={blog3Image} alt="" />
+                <p className="article_title">"האנגלית כשפת מפתח להצלחה"</p>
+                <p className="article_author">חשיבותה של השפה האנגלית בהשגת הצלחה, קידום אישי ומקצועי.</p>
                 </div>
-                <div className="article">
-                <img className="image_article" src="https://img.freepik.com/free-photo/wing-airplane-flying-sea-island_1232-1560.jpg?w=1380&t=st=1675791546~exp=1675792146~hmac=3ebffffd547cf15c7e9a233a13c8a0b63de4c80e16e75fcf39fed4fb9953196c" alt="" />
-                <p className="article_title">"Discover Adventure: Unforgettable Vacations"</p>
-                <p className="article_author">A guide to adventure-filled vacation destinations.</p>
+                <div className="article" onClick={() => navigate("/blog-post-2")}>
+                <img className="image_article" src={blog2Image} alt="" />
+                <p className="article_title">"הדרך הקלה ביותר ללמוד אנגלית"</p>
+                <p className="article_author">גישה פשוטה ויעילה ללימוד אנגלית, שניתן להבין וליישם בקלותם.</p>
                 </div>
-                <div className="article">
-                <img className="image_article" src="https://img.freepik.com/free-photo/beautiful-girl-standing-viewpoint-koh-nangyuan-island-near-koh-tao-island-surat-thani-thailand_335224-1085.jpg?w=1380&t=st=1675791615~exp=1675792215~hmac=41797ef143e86c1da5e89ea583a9be50fc68f644a00f3b92e7d8849b428dbca0" alt="" />
-                <p className="article_title">"Relax and Rejuvenate: Dream Vacations"</p>
-                <p className="article_author">A guide to tranquil vacation destinations for relaxation.</p>
+                <div className="article" onClick={() => navigate("/blog-post-1")}>
+                <img className="image_article" src={blog1Image} alt="" />
+                <p className="article_title">"טיפים לשיפור האנגלית שלך"</p>
+                <p className="article_author">עצות שימושיות לייעול תהליך למידת השפה האנגלית.</p>
                 </div>
 
             </div>

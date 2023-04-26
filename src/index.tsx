41fc3917de772cadfd2d0b52d0c22d3c788806d9
harelsarag7 <112413505+harelsarag7@users.noreply.github.com>
2023-04-26 21:45:42 +0300

    connected analytics

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import { store } from './App/store';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ScrollToTop from "./helpers/ScrollToTop";
+import ReactGA from "react-ga4";
 
 function App() {
   const webMode = useSelector((state: any) => state.chosenMode.toggle);
@@ -26,10 +27,19 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+ReactGA.initialize("G-T88M5S5TSF");
+
 root.render(
   <Provider store={store}>
     <App />
   </Provider>
 );
 
-reportWebVitals();
+const SendAnalytics = ()=> {
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
+}
+
+reportWebVitals(SendAnalytics);
